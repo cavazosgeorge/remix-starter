e4b8e30b3c0535cc7e6e08e3208e18f056355d0d
George Cavazos <cavazosgeorge@yahoo.com>
2022-05-10 14:41:22 -0400

    Imported Extended Colors && added to tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./app/**/*.tsx"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'current',
+        gray: colors.neutral,
+        indigo: colors.indigo,
+        purple: colors.purple,
+    }
   },
-  plugins: []
-};
+    plugins: []
+  }
+}
